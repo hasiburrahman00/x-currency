7d8cc71c7133e81c4a1f8f3c1f0f0f88d669bf29
hasibur_rahman <hasiburuoda@gmail.com>
2023-08-26 11:09:42 +0600

    x currency banner part design done

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./**/*.{html,js}"],
+    theme: {
+        container: {
+            center: true,
+            padding: '15px',
+            screens: {
+                sm: '575px',
+                md: '768px',
+                lg: '991px',
+                xl: '1280px'
+            }
+        },
+        extend: {
+            colors: {
+                primary: '#2E60E1',
+                secondary: '#575757',
+            },
+            boxShadow: {
+                'btn': '0px 1px 0px 0px #092E97'
+            },
+            dropShadow: {
+                'counter': '0px 8px 24px rgba(0, 0, 0, 0.08'
+            },
+            backgroundImage: {
+                ['btn-bg']: ' linear-gradient(180deg, #517DFC 0%, #0A42C6 100%)',
+                ['banner-bg']: 'url(../src/images/background-img.png)'
+            },
+
+        },
+    },
+    plugins: [],
+}
+
