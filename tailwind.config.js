74ed32e267d6565f203b085be1e19fe47e7a9a62
hasibur_rahman <hasiburuoda@gmail.com>
2023-09-09 16:46:05 +0600

    some bug fixed done

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,18 +15,24 @@ module.exports = {
         extend: {
             colors: {
                 primary: '#2E60E1',
-                secondary: '#575757',
+                secondary: '#575757'
             },
             boxShadow: {
-                'btn': '0px 1px 0px 0px #092E97'
+                'btn': '0px 1px 0px 0px #092E97',
+                'community': '0px 1px 0px 0px #092E97',
             },
             dropShadow: {
-                'counter': '0px 8px 24px rgba(0, 0, 0, 0.08'
+                'counter': '0px 8px 24px rgba(0, 0, 0, 0.08)'
             },
             backgroundImage: {
                 ['btn-bg']: ' linear-gradient(180deg, #517DFC 0%, #0A42C6 100%)',
-                ['banner-bg']: 'url(../src/images/background-img.png)'
+                ['banner-bg']: 'url(../src/images/background-img.png)',
+                ['community-bgImg']: 'url(../src/images/doatkolom-community.png)',
+                ['community-bgcolor']: 'linear-gradient(180deg, #517DFC 0%, #0A42C6 100%)'
             },
+            fontFamily: {
+                'lato': ['Lato']
+            }
 
         },
     },
